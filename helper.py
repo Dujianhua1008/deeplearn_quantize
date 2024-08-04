5bbc28ef38f7f8005c2c30a792a3fe485b304834
tulipdu1008 <tulipdu1008@gmail.com>
2024-08-04 21:18:25 +0800

    update helper

--- helper.py
+++ helper.py
@@ -3,6 +3,10 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import torch
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
 def plot_matrix(tensor, ax, title, vmin=0, vmax=1, cmap=None):
     """
     Plot a heatmap of tensors using seaborn
@@ -38,6 +42,8 @@ def plot_quantization_errors(original_tensor, quantized_tensor, dequantized_tens
     fig.tight_layout()
     plt.show()
 
+def quantization_error(dequantized_tensor,r_tensor , dtype=torch.int8):
+    return (dequantized_tensor-r_tensor).square().mean()
 
 ########## Functions from Linear Quantization I (Part 1)
 def linear_q_with_scale_and_zero_point(tensor, scale, zero_point, dtype = torch.int8):
@@ -54,3 +60,184 @@ def linear_dequantization(quantized_tensor, scale, zero_point):
     return scale * (quantized_tensor.float() - zero_point)
 
 #############
+def get_q_scale_and_zero_point(tensor, dtype=torch.int8):
+    
+    q_min, q_max = torch.iinfo(dtype).min, torch.iinfo(dtype).max
+    r_min, r_max = tensor.min().item(), tensor.max().item()
+
+    scale = (r_max - r_min) / (q_max - q_min)
+
+    zero_point = q_min - (r_min / scale)
+
+    # clip the zero_point to fall in [quantized_min, quantized_max]
+    if zero_point < q_min:
+        zero_point = q_min
+    elif zero_point > q_max:
+        zero_point = q_max
+    else:
+        # round and cast to int
+        zero_point = int(round(zero_point))
+    
+    return scale, zero_point
+
+def linear_quantization(tensor, dtype=torch.int8):
+    scale, zero_point = get_q_scale_and_zero_point(tensor, 
+                                                   dtype=dtype)
+    
+    quantized_tensor = linear_q_with_scale_and_zero_point(tensor,
+                                                          scale, 
+                                                          zero_point, 
+                                                          dtype=dtype)
+    
+    return quantized_tensor, scale , zero_point
+### Linear Quantization II (symmetric vs asymmetric)
+def get_q_scale_symmetric(tensor, dtype=torch.int8):
+    r_max = tensor.abs().max().item()
+    q_max = torch.iinfo(dtype).max
+
+    # return the scale
+    return r_max/q_max
+
+# L3_linear_II_per_channel
+def linear_q_symmetric_per_channel(r_tensor, dim, dtype=torch.int8):
+    
+    output_dim = r_tensor.shape[dim]
+    # store the scales
+    scale = torch.zeros(output_dim)
+
+    for index in range(output_dim):
+        sub_tensor = r_tensor.select(dim, index)
+        scale[index] = get_q_scale_symmetric(sub_tensor, dtype=dtype)
+
+    # reshape the scale
+    scale_shape = [1] * r_tensor.dim()
+    scale_shape[dim] = -1
+    scale = scale.view(scale_shape)
+    quantized_tensor = linear_q_with_scale_and_zero_point(
+        r_tensor, scale=scale, zero_point=0, dtype=dtype)
+   
+    return quantized_tensor, scale
+
+# L3_linear_II_per_group
+def linear_dequantization_per_group(quantized_tensor, scale, 
+                                    group_size):
+    
+    q_shape = quantized_tensor.shape
+    quantized_tensor = quantized_tensor.view(-1, group_size)
+    
+    dequantized_tensor = linear_dequantization(quantized_tensor, 
+                                               scale, 0)
+    
+    dequantized_tensor = dequantized_tensor.view(q_shape)
+    
+    return dequantized_tensor
+
+def get_q_scale_symmetric(tensor, dtype=torch.int8):
+    r_max = tensor.abs().max().item()
+    q_max = torch.iinfo(dtype).max
+
+    # return the scale
+    return r_max/q_max
+
+def linear_q_symmetric(tensor, dtype=torch.int8):
+    scale = get_q_scale_symmetric(tensor)
+    
+    quantized_tensor = linear_q_with_scale_and_zero_point(tensor,
+                                                     scale=scale,
+                   # in symmetric quantization zero point is = 0    
+                                                    zero_point=0,
+                                                      dtype=dtype)
+    
+    return quantized_tensor, scale
+
+def w8_a16_forward(weight, input, scales, bias=None):
+    
+    casted_weights = weight.to(input.dtype)
+    output = F.linear(input, casted_weights) * scales
+    
+    if bias is not None:
+        output = output + bias
+      
+    return output
+class W8A16LinearLayer(nn.Module):
+    
+    def __init__(self, in_features, out_features, 
+                 bias=True, dtype=torch.float32):
+        super().__init__()
+        
+        
+        self.register_buffer(
+            "int8_weights",
+            torch.randint(
+                -128, 127, (out_features, in_features), dtype=torch.int8
+            )
+        )
+        
+        self.register_buffer("scales", 
+                             torch.randn((out_features), dtype=dtype))
+        
+        if bias:
+            self.register_buffer("bias", 
+                                 torch.randn((1, out_features), 
+                                             dtype=dtype))
+        
+        else:
+            self.bias = None
+
+    def quantize(self, weights):
+        w_fp32 = weights.clone().to(torch.float32)
+
+        scales = w_fp32.abs().max(dim=-1).values / 127
+        scales = scales.to(weights.dtype)
+
+        int8_weights = torch.round(weights
+                        /scales.unsqueeze(1)).to(torch.int8)
+
+        self.int8_weights = int8_weights
+        self.scales = scales
+    
+    def forward(self, input):
+        return w8_a16_forward(self.int8_weights, 
+                              input, self.scales, self.bias)   
+        
+def replace_linear_with_target(module, 
+                               target_class, module_name_to_exclude):
+    for name, child in module.named_children():
+        if isinstance(child, nn.Linear) and not \
+          any([x == name for x in module_name_to_exclude]):
+            old_bias = child.bias
+
+            new_module = target_class(child.in_features, 
+                                      child.out_features, 
+                                      old_bias is not None, 
+                                      child.weight.dtype)
+            setattr(module, name, new_module)
+            if old_bias is not None:
+              getattr(module, name).bias = old_bias
+        else:
+            # Recursively call the function for nested modules
+            replace_linear_with_target(
+                child, target_class, module_name_to_exclude)
+            
+def replace_linear_with_target_and_quantize(module, 
+                               target_class, module_name_to_exclude):
+    for name, child in module.named_children():
+        if isinstance(child, nn.Linear) and not \
+        any([x == name for x in module_name_to_exclude]):
+            old_bias = child.bias
+            old_weight = child.weight
+
+            new_module = target_class(child.in_features, 
+                                      child.out_features, 
+                                      old_bias is not None, 
+                                      child.weight.dtype)
+            setattr(module, name, new_module)
+
+            getattr(module, name).quantize(old_weight)
+            
+            if old_bias is not None:
+              getattr(module, name).bias = old_bias
+        else:
+            # Recursively call the function for nested modules
+            replace_linear_with_target_and_quantize(child, 
+                     target_class, module_name_to_exclude) 
